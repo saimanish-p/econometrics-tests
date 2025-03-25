6a6ce0c2c99fb2294f3548a1c2fd7a4cb172ac8c
Saimanish Prabhakar <161119631+saimanish-p@users.noreply.github.com>
2025-03-25 12:16:40 +0400

    Update Diks-Panchenko test.R

--- Diks-Panchenko test.R
+++ Diks-Panchenko test.R
@@ -5,7 +5,7 @@ library(vars)
 library(haven)
 
 # Loading Stata dataset
-Model_3 <- read_dta("/Users/saimanishprabhakar/Desktop/Dissertation/Dissertation Data and Estimation/STATA Dissertation files/STATA data files/Model_3 (USEPU Endog & VIX Exog).dta")
+Model_3 <- read_dta("Path to file")
 
 # Checking for missing values and removing them if any
 colSums(is.na(Model_3))
